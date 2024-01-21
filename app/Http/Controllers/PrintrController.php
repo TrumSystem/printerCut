a76f61932e15d4ed02b28e688abb45bcc0d8d316
Trumsystem <trumsystem@gmail.com>
2024-01-21 17:29:01 -0300

    teste Produrcao

--- app/Http/Controllers/PrintrController.php
+++ app/Http/Controllers/PrintrController.php
@@ -16,6 +16,53 @@ class PrintrController extends Controller
         $this->printer = new Printer($connector);
     }
 
+    private function formatarCelular($celular)
+    {
+        // Remove qualquer caractere não numérico
+        $celular = preg_replace("/[^0-9]/", "", $celular);
+
+        // Verifica se o número de celular tem 11 dígitos (com o DDD)
+        if (strlen($celular) == 11) {
+            // Verifica se o número já está formatado no formato (XX) 9XXXX-XXXX
+            if (preg_match("/^\d{2}\s9\d{4}-\d{4}$/", $celular)) {
+                return $celular; // Já está formatado, retorna o mesmo número
+            } else {
+                return '(' . substr($celular, 0, 2) . ') ' . substr($celular, 2, 5) . '-' . substr($celular, 7, 4);
+            }
+        } else {
+            return false; // Número de celular inválido
+        }
+    }
+
+    private function formatarDocumento($documento)
+    {
+        // Remove qualquer caractere não numérico
+        $documento = preg_replace("/[^0-9]/", "", $documento);
+
+        // Verifica se o documento é um CPF (11 dígitos) ou CNPJ (14 dígitos)
+        if (strlen($documento) == 11) {
+            return substr($documento, 0, 3)
+                . '.'
+                . substr($documento, 3, 3)
+                . '.'
+                . substr($documento, 6, 3)
+                . '-'
+                . substr($documento, 9, 2);
+        } elseif (strlen($documento) == 14) {
+            return substr($documento, 0, 2)
+                . '.'
+                . substr($documento, 2, 3)
+                . '.'
+                . substr($documento, 5, 3)
+                . '/'
+                . substr($documento, 8, 4)
+                . '-'
+                . substr($documento, 12, 2);
+        } else {
+            return false; // Documento inválido
+        }
+    }
+
     public function cupon(Request $request)
     {
         try {
@@ -28,17 +75,17 @@ class PrintrController extends Controller
             $printer->setTextSize(2, 2);
             $printer->text("MI JP\n");
             $printer->setTextSize(1, 1);
-            $printer->text("20/01/2024 11:46 am\n");
+            $printer->text(date("d/m/Y H:i:s", strtotime($request->created_at)) . "\n");
             $printer->feed();
             // Detalhes do Cliente
             $printer->setPrintLeftMargin(16 * 2);
             $printer->setJustification(Printer::JUSTIFY_LEFT);
 
-            $printer->text($this->left('Cliente: ISAAC DA SILVA VINCENTE') . "\n");
-            $printer->text($this->left("CPF/CNPJ: 000.000.000-00") . "\n");
-            $printer->text($this->left("Tel: (83) 0000-0000") . "\n");
-            $printer->text($this->left("Vendedor: Eduardo") . "\n");
-            $printer->text($this->left("Venda nº 000000") . "\n");
+            $printer->text($this->left("Cliente: $request->cliente") . "\n");
+            $printer->text($this->left("CPF/CNPJ: " . $this->formatarDocumento($request->cpf)) . "\n");
+            $printer->text($this->left("Tel: " . $this->formatarCelular($request->contato)) . "\n");
+            $printer->text($this->left("Atendente: " . $request->atendente) . "\n");
+            $printer->text($this->left("Venda nº " . $request->numeroOs) . "\n");
             $printer->text($traco);
 
             $printer->text($this->left("descricao", 14));
@@ -46,16 +93,15 @@ class PrintrController extends Controller
             $printer->text($this->right("Total", 16) . "\n");
             $printer->text($traco);
             // Item
-            $descricao = "Redmi Note 128 GB PRETO";
-            $quantidade = $request->quantidade . "X";
-            $unitario = "R$ " . number_format($request->valor, 2, ',', '.');
-            $total = "R$ " . number_format($request->valor * $request->quantidade, 2, ',', '.');
+            $quantidade = $request->quantidade ?? 1 . "X";
+            $unitario = "R$ " . number_format($request->valorTotal, 2, ',', '.');
+            $total = "R$ " . number_format($request->valorTotal * $request->quantidade ?? 1, 2, ',', '.');
 
             // Calcula a quantidade de espaços necessários para alinhar
 
             //inicio foreach
             $printer->setEmphasis(true);
-            $printer->text($this->left($descricao) . "\n");
+            $printer->text($this->left($request->modelo . $request->cor) . "\n");
             $printer->setEmphasis(false);
             $printer->text($this->left($quantidade, 14));
             $printer->text($this->center($unitario, 14));
@@ -76,7 +122,7 @@ class PrintrController extends Controller
 
             $printer->text($traco);
             $printer->setEmphasis(true);
-            $totalPagar = ($request->taxa ?? 0.00) + ($request->valor * $request->quantidade);
+            $totalPagar = ($request->taxa ?? 0.00) + ($request->valorTotal * $request->quantidade ?? 1);
             $printer->text($this->left('Total a Pagar ', 22));
             $printer->text($this->right("R$ " . number_format($totalPagar, 2, ',', '.'), 22) . "\n");
             $printer->setEmphasis(false);
@@ -85,27 +131,31 @@ class PrintrController extends Controller
             $printer->text($this->center("---- Forma de Pagamento: ----") . "\n");
             // Detalhes de Pagamento
             //inicio foreach forma de pagamento
-            $printer->text($this->left('Dinheiro ', 22));
-            $printer->text($this->right("R$ 1.356,99", 22) . "\n");
+            $printer->text($this->left(
+                $request->formaPagamento == "Ávista" ? "Dinheiro" : $request->formaPagamento,
+                22
+            ));
+
+            $printer->text($this->right("R$ " . number_format($request->valorTotal, 2, ',', '.'), 22) . "\n");
             //fim oreach forma de pagamento
             $printer->text($traco);
 
             $printer->feed();
             $printer->text($this->center("---- Obs: ----") . "\n");
-            $printer->text($this->left("Entrada de R$350,00 a Vista (Dinheiro ou Pix) 10x de R$ 114,00\n"));
+            $printer->text($this->left($request->obs . "\n"));
             $printer->text($traco);
 
             // Informações do Produto
             $printer->feed();
-            $printer->text("IMEI 1: 000000000000000\n");
-            $printer->text("IMEI 2: 000000000000000\n");
+            $printer->text("IMEI 1: " . $request->imei1 . "\n");
+            $printer->text("IMEI 2: " . $request->imei2 . "\n");
             $printer->feed();
 
             // Endereço de Entrega
-            $printer->text("Endereco: Rua Deputado Iracio Bento\n");
-            $printer->text("Bairro: Tibiri 2\n");
+            $printer->text("Endereco: " . $request->rua . "\n");
+            $printer->text("Bairro: " . $request->bairro . "\n");
             $printer->text("CEP: 00000-000\n");
-            $printer->text("Cidade: Cidade\n");
+            $printer->text("Cidade: " . $request->cidade . "\n");
             $printer->text("UF: PB\n");
             $printer->text("Complemento: Principal Nova\n");
             $printer->text("Numero: 181 B\n");
